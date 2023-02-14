5836b5d161b52db19226d37b0d6841489243ee8b
Zivess <110631271+Zivess@users.noreply.github.com>
2023-02-14 02:12:26 -0300

    remove static page

--- curriculo/static/js/react/footer.js
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return(
-        <footer>
-            <div className="div-footer">
-                <small>© 2023 Jose development. All rights reserved.</small>
-            </div>
-        </footer>
-    )
-}
