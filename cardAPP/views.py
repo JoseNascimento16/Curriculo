6513f5bd5eb88ea8099310b4471567dc990dd274
Zivess <110631271+Zivess@users.noreply.github.com>
2023-02-14 02:11:16 -0300

    commit para producao

--- cardAPP/views.py
+++ cardAPP/views.py
@@ -1,6 +1,13 @@
+import os
+from django.http import FileResponse, Http404
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    # return render(request, 'index.html')
+
+    try:
+        return FileResponse(open('./static_dir/pdf/Resume-Jose-Nascimento.pdf', 'rb'), content_type='application/pdf')
+    except FileNotFoundError:
+        raise Http404()
