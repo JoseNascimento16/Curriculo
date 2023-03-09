d615c7dac04afc9bfb746887ff50d8b3beef055d
Zivess <110631271+Zivess@users.noreply.github.com>
2023-03-09 13:15:17 -0300

    atualização 09/03

--- cardAPP/views.py
+++ cardAPP/views.py
@@ -4,10 +4,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def index(request, **kwargs):
     # return render(request, 'index.html')
-
     try:
-        return FileResponse(open('./static_dir/pdf/Resume-Jose-Nascimento.pdf', 'rb'), content_type='application/pdf')
+        return FileResponse(open('./static_dir/pdf/Resume 2 - José Nascimento.pdf', 'rb'), content_type='application/pdf')
     except FileNotFoundError:
         raise Http404()
---
+++ curriculo/static/js/react/maincontent.js
@@ -0,0 +1,10 @@
+function MainContent(){
+    return(
+        <div className="div-main-content">
+            <img className="maincontent-img" src="/static/img/Curriculo-Jose-Nascimento.png" alt=""></img>
+            <div className="linkedin-div">
+                <img className="linkedin-img" src="/static/img/linkedin-icon.png" alt=""></img>
+            </div>
+        </div>
+    )
+}
